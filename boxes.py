c0c432ae83f10250414d61fda9e6f3e4152ee95b
Katesy Shepard <katesybby@byui.edu>
2023-01-10 22:09:52 -0700

    boxes.py

--- boxes.py
+++ boxes.py
@@ -2,3 +2,12 @@
 
 # the number of manufactured items
 # the number of items that the user will pack per box 
+
+import math
+
+items = int(input(f'Enter the number of items: '))
+capacity = int(input(f'Enter the number of items per box: '))
+
+box = math.ceil(items / capacity)
+
+print (f'\nFor {items} items, packing {capacity} items in each box, you will need {box} boxes.')
