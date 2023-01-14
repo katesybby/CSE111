84c89ad7d522b9b879bdda3c3f7bdd4216ce5e05
Katesy Shepard <katesybby@byui.edu>
2023-01-14 16:51:55 -0700

    refresh

---
+++ Lessons/review.py
@@ -0,0 +1,98 @@
+# Header
+'''
+Author:
+Date:
+Purpose:
+'''
+
+# pass
+x = 9
+if x < 10:
+    pass    # fills the need for an indented line
+
+# None
+x = None    # fills the need for 'something'
+if x == None:
+    pass
+
+# end
+for i in range(10):
+    print(i, end=' ')     # prints '0 1 2... 8 9'
+    print('X')       # prints '0 1 2... 8 9 X' 
+
+# sep
+for i in range(10):
+    print(i,i ** 2, sep='-', end=', ')     # prints '0-0, 1-1,... 9-9'
+
+# math
+import math
+print(math.pi)
+print(math.sqrt(138706))
+
+# random module
+import random
+print(random.randint(1, 100))   # randint
+names = ['bob', 'bily', 'bert']   # choice
+print(random.choice(names))
+
+# LOOPS
+# break statement
+for i in range(20):
+    if i == 10:
+        break
+    print(i, end=', ')   # prints '0... 9, '
+
+# continue statement
+for i in range(20):
+    if i == 10:
+        continue
+    print(i, end=', ')   # prints '0...9, 11...19' ten is skipped
+
+# using _ as a variable
+for _ in range(5):    # replaces i
+    print('Hello')
+
+# lower(), upper()
+(name.lower())
+(name.upper())
+
+# count()
+s = 'ergha;skfguhua;dfuvh;'
+print(s.count('e'))     # counts the numb of things in string
+
+# split()
+(name.split('i'))
+
+# replace()
+(name.split('bob', 'luke'))
+print(name)  
+
+# is functions
+(func.isalpha())
+(func.isnumeric())
+
+# lists
+print('abc' * 10)
+print('-' * 90)     # duplicates '-'
+print([0] * 10)     # '0, 0, ...'
+
+# list; names are 4x long
+names = ['moby', 'chase', 'bob']
+list = []
+for name in names:
+    if len(name) == 4:
+        print
+
+
+# EXAMPLE
+size = int(input('enter size of square: '))
+# goal: print 1 - size in decending order creating a box (i.e size = 16 > 4x4 box)
+for r in range(1, size + 1):
+    # 1 row
+    for i in range(r, size ** 2 + 1, size):
+        print(i, end=' ')
+    print()
+
+
+
+
--- tire_volume.py
+++ tire_volume.py
@@ -10,12 +10,10 @@ d is the diameter of the wheel in inches.
 
 import math 
 
-width = int(input(f"Enter the width of the tire in mm: "))
-aspect_ratio = int(input(f"Enter the aspect ratio of the tire: "))
-diameter = int(input(f"Enter the diameter of the wheel in inches: "))
+w = int(input(f"Enter the width of the tire in mm: "))
+a = int(input(f"Enter the aspect ratio of the tire: "))
+d = int(input(f"Enter the diameter of the wheel in inches: "))
 
+v = ((((math.pi) * ((w * w) * a)) * ((w * a) + (2540 * d))) / 10000000000)
 
-volume = complex(math.pi * width * width * aspect_ratio * (width * aspect_ratio + 2540 * diameter) / 10,000,000,000)
-
-
-print (f"The approximate volume is {volume:.2f} liters")
+print (f"The approximate volume is {v:.2f} liters")
