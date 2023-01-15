bb7ff7c837098e73fbce3d3d787892bca0c43d3c
Katesy Shepard <katesybby@byui.edu>
2023-01-14 17:47:30 -0700

    tire volume

--- tire_volume.py
+++ tire_volume.py
@@ -1,4 +1,5 @@
-# 01 Prove Milestone: Review Python
+# 01 Prove Milestone: Review Python 
+# --> 02 Prove: Calling Functions 
 
 """
 v is the volume in liters,
@@ -10,10 +11,27 @@ d is the diameter of the wheel in inches.
 
 import math 
 
-w = int(input(f"Enter the width of the tire in mm: "))
-a = int(input(f"Enter the aspect ratio of the tire: "))
-d = int(input(f"Enter the diameter of the wheel in inches: "))
+loop = 'y'      # extra feature: looped program
 
-v = ((((math.pi) * ((w * w) * a)) * ((w * a) + (2540 * d))) / 10000000000)
+while loop == 'y': 
+    w = int(input(f'\nEnter the width of the tire in mm: '))
+    a = int(input(f'Enter the aspect ratio of the tire: '))
+    d = int(input(f'Enter the diameter of the wheel in inches: '))
 
-print (f"The approximate volume is {v:.2f} liters")
+    v = math.pi * w * w * a * (w * a + 2540 * d) / 10000000000
+
+    print (f'The approximate volume is {v:.2f} liters')
+
+    # ----
+    from datetime import datetime
+
+    current_date_and_time = datetime.now()
+
+    with open("volumes.txt", "at") as volumes_file:
+
+        print(f'{current_date_and_time:%Y-%m-%d}, {w}, {a}, {d}, {v:.2f}', file = volumes_file)
+    # ----
+
+    loop = input(f'\nRESTART (y/n)? ')
+
+print (f'\nGoodbye. ')
